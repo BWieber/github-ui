71adf89e38a37a6914c6e3b28eaf53a9d06871a3
Brandon Wieber <bwieber@shyftanalytics.com>
2016-05-26 16:43:27 -0400

    Added redirection / data loop

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
+  var companies = [
+        {id: "emberjs"},
+        {id: "ember-cli"},
+        {id: "microsoft"},
+        {id: "yahoo"},
+        {id: "netflix"},
+        {id: "facebook"}
+  ];
+
 export default Ember.Route.extend({
+  model() {
+    return companies;
+  }
 });
