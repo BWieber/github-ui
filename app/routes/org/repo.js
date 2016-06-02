1e482dc25834cd7d2499f2c65002bedbbd11fb00
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-06-02 09:19:18 -0400

    Added API requests for issues and contributors

--- app/routes/org/repo.js
+++ app/routes/org/repo.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   redirect: function() {
     this.transitionTo('org.repo.issues');
+  },
+
+  model(params) {
+    let org = this.modelFor('org');
+
+    return $.get(`https://api.github.com/repos/${org.id}/${params.repoid}`).then(rawRepo => {
+      rawRepo.oldId = rawRepo.id;
+      rawRepo.id = rawRepo.name;
+      return rawRepo;
+    });
   }
 });
--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     let orgId = Ember.get(this.modelFor('org'), 'id');
+    
     return $.get(`https://api.github.com/orgs/${orgId}/repos`).then(rawRepos => {
       return rawRepos.map(rawRepo => {
         rawRepo.oldId = rawRepo.id;
